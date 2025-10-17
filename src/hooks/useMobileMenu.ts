128f55d7c722372fa05e4da2951a2fbf8c58f95e
Vinicius Assis <viniciusassis.dev@hotmail.com>
2025-10-17 08:41:38 -0300

    feat: add useMobileMenu custom hook

---
+++ src/hooks/useMobileMenu.ts
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+export const useMobileMenu = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  const closeMobileMenu = () => {
+    setIsMobileMenuOpen(false);
+  };
+
+  return {
+    isMobileMenuOpen,
+    toggleMobileMenu,
+    closeMobileMenu,
+  };
+};
