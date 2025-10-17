bd8f0296de4d1c3af41c22758061894bb9e2f6d2
Vinicius Assis <viniciusassis.dev@hotmail.com>
2025-10-17 08:41:01 -0300

    refactor: consolidate API types into single file

--- src/types/api.ts
+++ src/types/api.ts
@@ -13,3 +13,28 @@ export interface Category {
 export interface ApiResponse {
     categories: Category[];
 }
+
+export interface Subject {
+    id: string;
+    title: string;
+    image: string;
+    bgColor: string;
+    borderColor: string;
+    textColor: string;
+}
+
+export interface ReviewSubjectsResponse {
+    subjects: Subject[];
+}
+
+export interface Exam {
+    id: string;
+    category: string;
+    title: string;
+    daysAvailable: number;
+    textColor: string;
+}
+
+export interface ExamsResponse {
+    exams: Exam[];
+}
